eb9f160c623be32a36a36e1d039b2936de64bcd6
MOHANABHASKAR <65006239+MOHANABHASKAR@users.noreply.github.com>
2022-10-09 16:44:43 +0530

    Add files via upload

---
+++ question7evaluation/question7evaluation/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace question7evaluation
+{
+
+    public class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public override string ToString()
+        {
+            return Name + " - " + Age;
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+            Queue<Person> persons = new Queue<Person>();
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("Enter name of employee " + (i + 1) + " :");
+                string name = Console.ReadLine();
+                Console.WriteLine("Enter age of employee " + (i + 1) + " :");
+                int age = int.Parse(Console.ReadLine());
+                persons.Enqueue(new Person()
+                {
+                    Name = name,
+                    Age = age
+                });
+            }
+            Console.WriteLine("\nPrinting all employee details:");
+            Console.WriteLine("Name - Age");
+            foreach (Person p in persons)
+            {
+                Console.WriteLine(p.ToString());
+            }
+
+
+        }
+    }
+}
+
+
