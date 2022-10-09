c18e4df7b217ea4b910c089060102c106f4f914d
MOHANABHASKAR <65006239+MOHANABHASKAR@users.noreply.github.com>
2022-10-09 16:49:55 +0530

    Add files via upload

---
+++ interfaceevaluation/interfaceevaluation/Program.cs
@@ -0,0 +1,50 @@
+﻿using interfaceevaluation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace interfaceevaluation
+{
+    public interface Ivehicle
+    {
+         void drive();
+        bool refuel(int amount);
+
+    }
+    public class car : Ivehicle
+    {
+       public int fuel = 0;
+        public void drive()
+        {
+            if (fuel > 0)
+            {
+                Console.WriteLine("car is driving");
+            }
+
+        }
+        public bool refuel(int a)
+        {
+            fuel = fuel + a;
+            if (a > 0) { Console.WriteLine("fuel is there"); }
+            
+               
+            
+                return true;
+            
+        }
+    }
+    
+
+    }
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+        car c = new car();
+        c.refuel(Convert.ToInt32(Console.ReadLine()));
+        c.drive();
+        }
+    }
+
