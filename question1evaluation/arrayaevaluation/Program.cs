4d31f73223a0cbeecb2463549fab199ffceda4d0
MOHANABHASKAR <65006239+MOHANABHASKAR@users.noreply.github.com>
2022-10-09 16:35:53 +0530

    Add files via upload

---
+++ question1evaluation/arrayaevaluation/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace arrayaevaluation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr;
+            arr = new int[5];
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = int.Parse(Console.ReadLine());
+            }
+             Array.Sort(arr);
+            int min = arr[0];
+            int max = arr[arr.Length - 1];
+
+
+            Console.WriteLine(min);
+            Console.WriteLine(max);
+
+        }
+    }
+}
