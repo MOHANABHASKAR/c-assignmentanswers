b4fc9b88124805510e9d2d9a31cf2422fec0d287
MOHANABHASKAR <65006239+MOHANABHASKAR@users.noreply.github.com>
2022-10-09 16:46:16 +0530

    Add files via upload

---
+++ multiplicationtable/multiplicationtable/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace multiplicationtable
+{
+    internal class Program
+    { 
+        
+        static void Main(string[] args)
+        {
+            int a = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i < 100; i++)
+            {
+                Console.WriteLine(a + "*" + i + "= " + a * i);
+            }
+            int factorial(int x)
+            {
+                int ans = 1;
+                while (x > 1)
+                {
+                    ans = ans * x;
+                    x--;
+                }
+                return ans;
+            }
+            Console.WriteLine("the factorial of {0}",a);
+            Console.WriteLine(factorial(a));
+
+            }
+
+        }
+    }
+
